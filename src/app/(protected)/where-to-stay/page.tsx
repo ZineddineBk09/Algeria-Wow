ea1160b203194b4367d146f7142732ee79726398
ZineddineBk09 <eino1209@gmail.com>
2024-07-20 14:44:34 +0100

    add user location map component

--- src/app/(protected)/where-to-stay/page.tsx
+++ src/app/(protected)/where-to-stay/page.tsx
@@ -1,6 +1,8 @@
-import { ArrowRightIcon, CalendarIcon, LocateIcon, UserIcon } from "lucide-react";
+import { ArrowRightIcon, CalendarIcon, MapPin, UserIcon } from "lucide-react";
+import { useState } from "react";
 import AdBanner from "~/components/pages/home/ad-banner";
 import WhereToStayPage from "~/components/pages/where-to-stay";
+import UserLocation from "~/components/pages/where-to-stay/filters/map";
 import { DatePicker } from "~/components/ui/date-picker";
 import { Input } from "~/components/ui/input";
 import { PlacesContextProvider } from "~/context/places";
@@ -28,28 +30,16 @@ export default function WhereToStay() {
 function SearchBar() {
   return (
     <>
-      <div className="flex w-full items-start space-x-4">
-        <div className="relative">
-          <img
-            src="/placeholder.svg"
-            alt="Map thumbnail"
-            className="h-32 w-72 rounded-lg object-cover"
-          />
-          <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-black bg-opacity-50">
-            <span className="font-semibold text-white">
-              View on map{" "}
-              <ArrowRightIcon className="ml-1 inline-block h-4 w-4" />
-            </span>
-          </div>
-        </div>
+      <div className="flex w-full items-start space-x-6">
+        <UserLocation />
 
         <div className="">
           <h2 className="mb-4 text-2xl font-bold text-blue-900">
             Where to stay in Algeria
           </h2>
-          <div className="grid grid-cols-9 items-center justify-between space-x-4 rounded-lg bg-gray-50 p-4 drop-shadow-lg">
+          <div className="grid grid-cols-9 items-center justify-between space-x-4 rounded-xl bg-white p-3 shadow-inner drop-shadow-md">
             <div className="col-span-2 flex items-center space-x-2 pl-4">
-              <LocateIcon className="h-6 w-6 text-yellow-500" />
+              <MapPin className="h-8 w-8 text-yellow-500" />
               <Input
                 type="text"
                 placeholder="City"
@@ -57,23 +47,23 @@ function SearchBar() {
               />
             </div>
             <div className="col-span-2 flex items-center space-x-2 border-l pl-4">
-              <CalendarIcon className="h-6 w-6 text-yellow-500" />
-              <DatePicker />
+              <CalendarIcon className="h-8 w-8 text-yellow-500" />
+              <DatePicker placeholder="Check in" />
             </div>
             <div className="col-span-2 flex items-center space-x-2 border-l pl-4">
-              <CalendarIcon className="h-6 w-6 text-yellow-500" />
-              <DatePicker />
+              <CalendarIcon className="h-8 w-8 text-yellow-500" />
+              <DatePicker placeholder="Check out" />
             </div>
 
             <div className="col-span-2 flex items-center space-x-2 border-l pl-4">
-              <UserIcon className="h-6 w-6 text-yellow-500" />
+              <UserIcon className="h-8 w-8 text-yellow-500" />
               <Input
                 type="text"
                 placeholder="Persons"
                 className="border-none bg-transparent p-2 focus:outline-none"
               />
             </div>
-            <button className="h-11 rounded-md bg-primary-yellow px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-primary-yellow/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-yellow lg:h-12">
+            <button className="h-10 rounded-lg bg-primary-yellow px-8 text-sm font-semibold text-gray-50 shadow transition-colors hover:bg-primary-yellow/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary-yellow lg:h-11">
               Search
             </button>
           </div>
