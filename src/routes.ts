f4095217b4d664b026336a83f34d2ffaadf7b49e
ZineddineBk09 <eino1209@gmail.com>
2024-07-22 08:48:14 +0100

    app routes

---
+++ src/routes.ts
@@ -0,0 +1,5 @@
+export const privateRoutes = ["/where-to-stay", "/what-to-do", "/where-to-eat"];
+export const authRoutes = ['/'];
+export const DEFAULT_REDIRECT_LOGIN_URL = ['/'];
+export const DEFAULT_REDIRECT_HOME_URL = ['/where-to-stay'];
+
