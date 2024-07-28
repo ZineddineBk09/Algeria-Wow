34b8715b5f16e8fa648bfe7b71b1ae1898faae3d
ZineddineBk09 <eino1209@gmail.com>
2024-07-28 08:13:18 +0100

    refactor

--- src/components/pages/what-to-do/filters/map.tsx
+++ src/components/pages/what-to-do/filters/map.tsx
@@ -53,7 +53,7 @@ export default function UserLocation() {
           }}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
         </MapContainer>
